22b6ddbbd17e6de3302c1de8951326768a52fd7f
Lucas <lucasspindolafernandes196@hotmail.com>
2023-01-12 11:38:35 -0300

    feature: add route get, verification middleware if user is employed and implementation of request(@types)

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -2,10 +2,18 @@ import {
   createNewUserController,
   listAllUsersController,
 } from "../../controllers/users/users.controller";
+import employeePrivateRouteCheckMiddlewar from "../../middlewares/employeePrivateRouteCheck.middlewar";
+
+import { userTokenVerificationMiddleware } from "../../middlewares";
 import { Router } from "express";
 const userRoutes = Router();
 
 userRoutes.post("", createNewUserController);
-userRoutes.get("", listAllUsersController);
+userRoutes.get(
+  "",
+  userTokenVerificationMiddleware,
+  employeePrivateRouteCheckMiddlewar,
+  listAllUsersController
+);
 
 export default userRoutes;
