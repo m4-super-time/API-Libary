6ea91ce2f319aebeddcf40facb55bce274234da9
Lucas <lucasspindolafernandes196@hotmail.com>
2023-01-14 11:31:49 -0300

    update branch for develp

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import { handleAppError } from "./errors";
 import { sessionRouter } from "./routes";
 import userRoutes from "./routes/users";
 import { categoriesRoutes } from "./routes/categories";
+import profileRoutes from "./routes/profile";
+import { createNewBookController } from "./controllers/books/books.controller";
 
 const app = express();
 app.use(express.json());
@@ -12,7 +14,8 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/login", sessionRouter);
 app.use("/categories", categoriesRoutes);
-
+app.use("/profile", profileRoutes);
+app.use("/books", createNewBookController);
 app.use(handleAppError);
 
 export default app;
--- src/controllers/books/books.controller.ts
+++ src/controllers/books/books.controller.ts
@@ -9,14 +9,3 @@ export const createNewBookController = async (req: Request, res: Response) => {
 
   return res.status(201).json(registerBook);
 };
-
-// name: string;
-
-// @Column("decimal", { precision: 12, scale: 2 })
-// price: number;
-
-// @Column()
-// author: string;
-
-// @Column()
-// synopsis: string;
